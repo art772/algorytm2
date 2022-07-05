bd45f013b5064811f0a0c7d71a60ecf492f2d792
art772 <art772@wp.pl>
2022-07-05 10:13:04 +0200

    Update Program.cs
    Checking loop whether the lesson is over

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
-int startLesson;
-int stopLesson;
+//List<(int, int)> lessonList = new List<(int, int)>() { (30, 75), (0, 50), (60, 150) };  //  2
+//List<(int, int)> lessonList = new List<(int, int)>() { (0, 75), (0, 35), (40, 90), (20, 70) };    //  3
+//List<(int, int)> lessonList = new List<(int, int)>() { (0, 40), (0, 20), (20, 40), (20, 60), (30, 60), (30, 50), (40, 100) }; //  4
+List<(int, int)> lessonList = new List<(int, int)>() { (0, 40), (0, 20), (20, 30), (20, 60), (30, 60) };  //  3 
 
-//List<(int, int)> lessonList = new List<(int, int)>() { (30, 75), (0, 50), (60, 150) };
-//List<(int, int)> lessonList = new List<(int, int)>() { (0, 75), (0, 35), (40, 90), (20, 70) };
-List<(int, int)> lessonList = new List<(int, int)>() { (0, 40), (0, 20), (20, 40), (20, 60), (30, 60), (30, 50), (40, 100) };
 List<(int, int)> classList = new List<(int, int)>();
 
 lessonList.Sort();
+
+int startLesson;
+int stopLesson;
 
 int i = 0;
 
@@ -23,6 +25,18 @@ while (i < lessonList.Count)
     }
     else
     {
+        /*
+         * Sprawdzanie czy jakaś lekcja się skończyła.
+         * Czas rozpęcięcia kolejnej lekcji to aktualna godzina
+         * Jeżeli czas zakończenia jakiejś lekcji jest równy rozpoczęciu innej to usuń lekcję z listy klass
+         */
+        foreach (var c in classList.ToList())
+        {
+            if (c.Item2 == startLesson)
+            {
+                classList.Remove((c.Item1, c.Item2));
+            }
+        }
         foreach (var c in classList.ToList())
         {
             if (startLesson < c.Item2)
@@ -30,8 +44,8 @@ while (i < lessonList.Count)
                 classList.Add((startLesson, stopLesson));
                 break;
             }
-            else if (startLesson == c.Item2) {
-                classList.Remove((c.Item1, c.Item2));
+            else if (startLesson == c.Item2)
+            {
                 classList.Add((startLesson, stopLesson));
                 break;
             }
